57d03a1e1810257779d123b1adfc3de8b3edb2ac
Eddie S <hotstreams7@yandex.ru>
2020-08-03 17:22:56 +0300

    vignette postprocess

--- shaders/postprocessing/postprocess.fs
+++ shaders/postprocessing/postprocess.fs
@@ -16,6 +16,10 @@ uniform float gamma;
 uniform bool tone_mapping;
 uniform float exposure;
 
+uniform bool vignette;
+uniform float vignette_radius;
+uniform float vignette_softness;
+
 void main()
 {
     vec3 scene_color = texture(image, fs_uv).rgb;
@@ -30,6 +34,12 @@ void main()
 
     if (gamma_correction) {
         scene_color = pow(scene_color, vec3(1.0 / gamma));
+    }
+
+    if (vignette) {
+        float len = length(gl_FragCoord.xy / textureSize(image, 0) - vec2(0.5));
+        float vig = smoothstep(vignette_radius, vignette_softness, len);
+        scene_color.rgb = mix(scene_color.rgb, scene_color.rgb * vig, 0.5);
     }
 
     color = vec4(scene_color, 1.0);
