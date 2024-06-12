016a3889e6d46803f01d2aa5886730d6f7678236
Tehsapper <tehsapper@mail.ru>
2024-06-12 22:57:35 +0900

    support Unicode in font atlas
    use glyph packing from stb for atlas generation

--- include/limitless/text/font_atlas.hpp
+++ include/limitless/text/font_atlas.hpp
@@ -27,7 +27,7 @@ namespace Limitless {
 
     class FontAtlas {
     private:
-        std::map<char, FontCharacter> chars;
+        std::map<uint32_t, FontCharacter> chars;
         std::shared_ptr<Texture> texture;
         FT_Face face {};
         uint32_t font_size;
@@ -36,16 +36,22 @@ namespace Limitless {
 
         bool isSynthetizedGlyph(uint32_t utf32_codepoint) const noexcept;
     public:
-        FontAtlas(const fs::path& path, uint32_t size);
+        FontAtlas(const fs::path& path, uint32_t pixel_size);
         ~FontAtlas();
 
         [[nodiscard]] auto getFontSize() const noexcept { return font_size; }
-        [[nodiscard]] const auto& getFontCharacter(char c) const { return chars.at(c); }
+        [[nodiscard]] const auto& getFontCharacter(uint32_t ucs) const { return chars.at(ucs); }
 
         [[nodiscard]] const auto& getTexture() const { return texture; }
 
+        /**
+         * Return vertices for UTF-8 encoded string.
+         */
         [[nodiscard]] std::vector<TextVertex> generate(const std::string& text) const;
 
+        /**
+         * Return bounding box for UTF-8 encoded string.
+         */
         [[nodiscard]] glm::vec2 getTextSize(const std::string& text) const;
 
         std::vector<TextVertex> getSelectionGeometry(std::string_view text, size_t begin, size_t end);
